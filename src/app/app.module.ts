0a374b65ace17c6a3cb9f3b9194e73132e4fb6ba
Carmelo Buelvas <cbuelvasc@gmail.com>
2022-07-18 18:38:39 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CounterComponent, HeroeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h1>{{ title }}</h1>
+    <h3>
+      Initial value: <strong>{{ initial }}</strong>
+    </h3>
+    <button (click)="accumulate(-initial)">-{{ initial }}</button>
+    <span>{{ number }}</span>
+    <button (click)="accumulate(initial)">+{{ initial }}</button>
+  `,
+})
+export class CounterComponent {
+  title: string = 'Counter App';
+  number: number = 2;
+  initial: number = 5;
+
+  accumulate(value: number) {
+    this.number += value;
+  }
+}
